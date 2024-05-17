828eb8bcb554f32a346700a29d1bd1a9add146e9
tunwit <tunwit2458@gmail.com>
2024-05-17 22:40:46 +0700

    fix bug on linkshortener

--- cogs/linkshortener.py
+++ cogs/linkshortener.py
@@ -9,6 +9,7 @@ from ui.language_respound import get_respound
 from ui.help_embed import embed as e
 from ui.button import buttin
 import requests
+import asyncio
 
 class linkshortenerAPI(commands.Cog):
     def __init__(self,bot):
@@ -25,11 +26,17 @@ class linkshortenerAPI(commands.Cog):
     async def linkshortener(self,interaction:discord.Interaction,url:str):
         await interaction.response.defer()
         if await self.check_ban(interaction.user.id):
-            result = requests.post('https://littleshort.vercel.app/api/link',json={"origin":'https://www.w3schools.com/python/ref_requests_post.asp'})
-            if result.status_code == 201:
-               await interaction.followup.send(f'https://littleshort.vercel.app/{result.json()["data"]["uniqueID"]}')
-            else:
-               await interaction.followup.send(f'There is an error occured status code {result.status_code}')
+          respound = get_respound(interaction.locale,"baned")
+          embed = createembed.baned(interaction,interaction.client,respound)
+          d = await interaction.followup.send(embed=embed)
+          await asyncio.sleep(5)
+          await d.delete()
+          return
+        result = requests.post('https://littleshort.vercel.app/api/link',json={"origin":url})
+        if result.status_code == 201:
+            await interaction.followup.send(f'https://littleshort.vercel.app/{result.json()["data"]["uniqueID"]}')
+        else:
+            await interaction.followup.send(f'There is an error occured status code {result.status_code}')
 
 async def setup(bot):
   await bot.add_cog(linkshortenerAPI(bot))
