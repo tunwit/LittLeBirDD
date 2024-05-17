f06d4d24b4205e78c6eee3f90be2ddad583198d0
tunwit <tunwit2458@gmail.com>
2024-05-17 21:36:04 +0700

    link shortener feature

---
+++ cogs/linkshortener.py
@@ -0,0 +1,35 @@
+from typing import List
+from colorama import Style
+import discord
+from discord.ext import commands
+import json
+from discord import app_commands
+from ui.embed_gen import createembed
+from ui.language_respound import get_respound
+from ui.help_embed import embed as e
+from ui.button import buttin
+import requests
+
+class linkshortenerAPI(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+
+    async def check_ban(self,v):
+      if self.bot.mango['ban'].find_one({'user_id':str(v)}):
+        return True
+      else:
+        return False
+      
+    @app_commands.command(name="linkshortener",description="shorten your link")
+    @app_commands.describe(url="link or url")
+    async def linkshortener(self,interaction:discord.Interaction,url:str):
+        await interaction.response.defer()
+        if await self.check_ban(interaction.user.id):
+            result = requests.post('https://littleshort.vercel.app/api/link',json={"origin":'https://www.w3schools.com/python/ref_requests_post.asp'})
+            if result.status_code == 201:
+               await interaction.followup.send(f'https://littleshort.vercel.app/{result.json()["data"]["uniqueID"]}')
+            else:
+               await interaction.followup.send(f'There is an error occured status code {result.status_code}')
+
+async def setup(bot):
+  await bot.add_cog(linkshortenerAPI(bot))
--- test.py
+++ test.py
@@ -1,1 +1,5 @@
-import lavalink
+import requests
+
+result = requests.post('https://littleshort.vercel.app/api/link',json={"origin":'https://www.w3schools.com/python/ref_requests_post.asp'})
+
+print(result.json()['data'])
