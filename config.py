72997918f98c59e11719a3c394fca9aa21b45733
tunwit <tunwit2458@gmail.com>
2024-06-06 22:11:35 +0700

    ability to use local lavalink

--- config.py
+++ config.py
@@ -2,6 +2,10 @@ import json
 from dotenv import load_dotenv
 import logging
 import os
+import requests
+import sys
+
+path = os.getcwd()
 
 logger = logging.getLogger('littlebirdd')
 with open("_config.json", "r") as f:
@@ -17,6 +21,43 @@ elif MODEL == 'test':
     logger.info('Load new .env.development')
 else:
     raise 'Unvalid bot MODEL'
+
+
+LOCAL_LAVALINK = config[MODEL]['local_lavalink']
+if LOCAL_LAVALINK:
+    if not os.path.exists(f"lavalink"):
+        os.makedirs(f"lavalink")
+    logger.info("using Local Lavalink")
+    if not os.path.isfile(f"lavalink\\Lavalink.jar"):
+        try:
+            logger.info('Downloading Lavalink.jar.')
+            response = requests.get('https://github.com/lavalink-devs/Lavalink/releases/download/4.0.5/Lavalink.jar', stream=True)
+            response.raise_for_status()
+            with open(f"lavalink\\Lavalink.jar", 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            logger.info('Lavalink success fully dowloaded')        
+        except requests.exceptions.RequestException as e:
+            logger.info(f'Fail to dowload Lavalike due to \n{e}')  
+            sys.exit()
+
+    if not os.path.isfile(f"lavalink\\application.yml"):
+        try:
+            logger.info('Downloading application.yml.')
+            response = requests.get('https://raw.githubusercontent.com/tunwit/Lavalink/main/application.yml', stream=True)
+            response.raise_for_status()
+            with open(f"lavalink\\application.yml", 'wb') as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    file.write(chunk)
+            logger.info('application.yml success fully dowloaded')        
+        except requests.exceptions.RequestException as e:
+            logger.info(f'Fail to dowload application.yml due to \n{e}')  
+            sys.exit()
+
+
+    if not os.path.isfile(f"start_lavalink.bat"):      
+        with open(f'start_lavalink.bat', "w") as bat_file:
+            bat_file.write(f"""start cmd.exe /k "cd lavalink && java -jar lavalink.jar""")   
 
 CONFIG = config[MODEL] 
 TOKEN = os.getenv('TOKEN')
